d7b6a95fe973d612e0f313a83d1eec60742f91e3
sagharnour <saghar.noursobhi@hyperisland.se>
2021-08-23 16:45:58 +0200

    mouseover and mouseout event on image gallery

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+    //testimonial mouseover mouseout
+    [...document.querySelectorAll('.figure')].forEach((item)=>{
+        
+        item.addEventListener('mouseover' , (e)=>{
+            let target =e.target
+            while(!target.classList.contains('figure')){
+                target = target.parentElement;
+            }
+            const image1 = target.querySelector('.figure-img');
+            let text = target.querySelector('.figure-text');
+            image1.style.opacity = '0.5';
+            text.style.display = 'block';
+
+            })
+
+        item.addEventListener('mouseout' , (e)=>{
+            let target = e.target;
+            if(!target.classList.contains('figure')){
+                target = target.parentElement;
+            }
+            const image2 = target.querySelector('.figure-img');
+            let text = target.querySelector('.figure-text');
+            image2.style.opacity = '1';
+            text.style.display = 'none'
+        })
+    })
+        
+    
+    
     let menuTrigger = document.querySelector('.menu-trigger-js');
     let menuPart = document.querySelector('.menu-js');
     let body = document.getElementsByTagName('BODY')[0];
